bc61cc32d9ab9fb8b90a62833649f36be0e57778
Harsh Sharma <sharmahs017@gmail.com>
2026-04-10 14:57:09 +0530

    fix the cors problem

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://meme-app-eight-eta.vercel.app"
+        "https://meme-app-eight-eta.vercel.app/api"
     ],
     credentials: true
 }));
